3301ba9055b5886158191ab6394b7cc12de5894d
farjana74 <csefarjana74@gmail.com>
2022-09-26 12:25:39 +0600

    aded components

---
+++ pages/Components/TermAndUse.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TermAndUse = () => {
+    return (
+        <div>
+            <h3 className='text-white'>term</h3>
+        </div>
+    );
+};
+
+export default TermAndUse;
